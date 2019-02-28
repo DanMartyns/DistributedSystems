749120de892134b10336fed38fb901b73df2a392
Daniel Martins <danielmcoelho@ua.pt>
2019-02-28 10:59:12 +0000

    Intervining Entities and Shared Regions updated

--- Assignment1/src/MainProgram/InterveningEntities/Customer.java
+++ Assignment1/src/MainProgram/InterveningEntities/Customer.java
@@ -1,10 +1,15 @@
 package MainProgram.InterveningEntities;
 
+import java.util.*;
 /**
  *
  * @author danielmartins
  */
 public class Customer extends Thread {
+    
+    private Random gerador = new Random();
+    private int NORMAL_LIFE = gerador.nextInt(60); // random number between 0 and 60
+    private int DECIDE_ON_REPAIR = gerador.nextInt(10); //random number between 0 and 10 to decide if he want repair de car or not 
     
     /* Means the Life Cycle of the Customer */
     @Override
--- Assignment1/src/SharedRegions/Lounge.java
+++ Assignment1/src/SharedRegions/Lounge.java
@@ -5,10 +5,24 @@
  */
 package SharedRegions;
 
+import java.util.*;
 /**
  *
  * @author danielmartins
  */
 public class Lounge {
+    private Queue<Integer> lounge = new LinkedList<Integer>();
     
+    // The customer go in to the lounge
+    public synchronized void add(){
+        
+    }
+    // The manager checks if there is someone in the lounge
+    public synchronized boolean isEmpty(){
+        return false;
+    }
+    //The Manager peeks the client from the lounge
+    public synchronized int peek(){
+        return 0;
+    }
 }
